4284b3217c159f6629a9c8a8cd9e75c7851d30e2
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-05-31 01:06:44 -0400

    Forgot to commit config

--- qmod/include/config.hpp
+++ qmod/include/config.hpp
@@ -3,8 +3,9 @@
 DECLARE_CONFIG(LiveStreamQuestConfig) {
   CONFIG_VALUE(sendScores, bool, "Automatically send scores to the server",
                true);
-  CONFIG_VALUE(frequency, int, "Frequency (in hz) to send player position updates to the server",
-               60);
-  CONFIG_VALUE(networkTransport, int, "Network transport",
-               0, "0 = TCP, 1 = UDP");
+  CONFIG_VALUE(
+      frequency, int,
+      "Frequency (in hz) to send player position updates to the server", 60);
+  CONFIG_VALUE(networkTransport, int, "Network transport", 0,
+               "0 = TCP, 1 = UDP");
 };
